5863b45fd998f586348925f6bb4b382c2d7ec020
Valencesama61 <valenceodounbourou@gmail.com>
2026-06-05 11:53:46 +0100

    Externalise l'URL de l'API via NEXT_PUBLIC_API_URL
    - baseURL axios lit la variable d'environnement au lieu de localhost:8080
    - Ajout de .env.example comme référence des variables requises

--- lib/axios.ts
+++ lib/axios.ts
@@ -4,7 +4,7 @@ import Cookies from 'js-cookie'
 const TOKEN_KEY = 'rapidgaz_token'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
